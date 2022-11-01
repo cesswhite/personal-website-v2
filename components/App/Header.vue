829c976a950c7dc65682ce50ca513d2846324f4e
cesswhite <acessloop@gmail.com>
2022-11-01 15:17:48 -0600

    [FIX] - Padding in mobile

--- components/App/Header.vue
+++ components/App/Header.vue
@@ -1,13 +1,13 @@
 <template>
   <header
-    class="bg-light-50/60 border-b-1 border-dark-900 w-full grid px-4 top-0 z-1 grid-cols-12 backdrop-blur-lg fixed items-center justify-between dark:bg-dark-900/80 dark:border-light-50">
+    class="bg-light-50/60 border-b-1 border-dark-900 w-full grid px-1 top-0 z-1 grid-cols-12 backdrop-blur-lg fixed items-center justify-between lg:px-4 dark:bg-dark-900/80 dark:border-light-50">
     <div class="border-r-1 border-dark-900 col-span-2 dark:border-light-50">
       <NuxtLink to="/">
         <AppLogo />
       </NuxtLink>
     </div>
     <div class="flex h-full border-r-1 border-dark-900 px-4 col-span-9 items-center dark:border-light-50">
-      <nav class="flex w-full text-dark-900 gap-2 items-center justify-start dark:text-light-50">
+      <nav class="flex w-full text-dark-900 gap-2 items-center justify-start overflow-x-scroll dark:text-light-50">
         <NuxtLink to="/about">
           About
         </NuxtLink>
